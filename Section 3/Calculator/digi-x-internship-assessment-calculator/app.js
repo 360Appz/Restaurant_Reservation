0475a8617338af7657fdd58915653378fe406f07
360Appz <cookiesbyjy@gmail.com>
2021-08-13 16:25:07 +0800

    Calculator commit

--- Section 3/Calculator/digi-x-internship-assessment-calculator/app.js
+++ Section 3/Calculator/digi-x-internship-assessment-calculator/app.js
@@ -8,7 +8,10 @@
   buttons.forEach(function(button){
     button.addEventListener('click', function(e){
       let value = e.target.dataset.num;
-      screen.value = value;
+      screen.value += value;
+      //Previously, the + was not there, it is required
+      //so that both the input value pn screen will be 
+      //concatenated for the eval() function to work
     })
   });
   
@@ -17,7 +20,12 @@
       screen.value = 'Please Enter a Value';
     } else {
       let answer = eval(screen.value);
-      screen.value += answer;
+      screen.value = answer;
+      //Previously, the + was there, it should
+      //be removed to to prevent concatenation of
+      //values after the eval() function is executed
+      //In this part, we are just assigning the answer to
+      //the screen.value not doing any operations
     }
   })
   
